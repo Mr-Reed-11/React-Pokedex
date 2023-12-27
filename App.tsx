bd425caeae44451547a76421949f1366e900976d
Vitor <vitorcesargoncalvesornelas@gmail.com>
2023-12-27 14:57:40 -0300

    feat: first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>ReactDex</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
